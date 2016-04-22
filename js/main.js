7d2c7db203be0029892619613e823267682b9bc3
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-04-22 16:38:16 -0400

    starting animation on beans

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // make a class for animating the kids on transform origin and middle/ large that is default applied
 // on click
-// 1) have a class removed from the kids , which that makes them  smaller and positioned left, make transition for the movement
+// 1) DONE!!! >>>>>>> have a class removed from the kids , which that makes them  smaller and positioned left, make transition for the movement
 // 2) add classes to the food circles and transition opacity in... make delay so one by one
+
+$('.kids').on('click', function () {
+  $('#second-protein-list').removeClass('unit-xs-hidden');
+});
 
 // listen for click on circle food
 // add class to transition opacity and animate downward
@@ -13,6 +17,11 @@
 // listen for click on the bean
 // add class to animate bean to new position
 // add class to animate slide of label and transition opacity
+
+$('.bean').on('click', function () {
+  $('this').addClass('bean-move');
+  $('this').removeClass('bean');
+});
 
 // waypoint the foot graph
 // for each of the lines, delay:
